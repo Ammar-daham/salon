85bba70b1af76162d7c4942038ad6e7522c98593
Ammar Daham <aljewaryammar@gmail.com>
2026-01-24 11:43:02 +0200

    salon-backend: created staff DAO

---
+++ src/main/java/com/example/salon/dao/StaffDao.java
@@ -0,0 +1,15 @@
+package com.example.salon.dao;
+
+import com.example.salon.model.Staff;
+import com.example.salon.model.User;
+
+import java.util.List;
+
+public interface StaffDao
+{
+    long addUser(Staff staff);
+    List<Staff> getAllStaff();
+    Staff getStaffById(int id);
+    long updateStaffById(long id, Staff staff);
+    long deleteStaffById(long id, Staff staff);
+}
---
+++ src/main/java/com/example/salon/dao/StaffDataAccessService.java
@@ -0,0 +1,45 @@
+package com.example.salon.dao;
+
+import com.example.salon.model.Staff;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.List;
+
+public class StaffDataAccessService implements StaffDao
+{
+    private final JdbcTemplate jdbcTemplate;
+
+    public StaffDataAccessService(JdbcTemplate jdbcTemplate)
+    {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public long addUser(Staff staff)
+    {
+        return 0;
+    }
+
+    @Override
+    public List<Staff> getAllStaff()
+    {
+        return List.of();
+    }
+
+    @Override
+    public Staff getStaffById(int id)
+    {
+        return null;
+    }
+
+    @Override
+    public long updateStaffById(long id, Staff staff)
+    {
+        return 0;
+    }
+
+    @Override
+    public long deleteStaffById(long id, Staff staff) {
+        return 0;
+    }
+}
